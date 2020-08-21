cdaf14c514e98f2645dcf764c63b6c9ff5a943a9
devteam2 <devteam2@digitalflexsolutions.com>
2020-08-21 18:17:44 +0300

    project state 21.08.2020

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('.products-section__slider').slick({
+    	slidesToShow: 3,
+  		slidesToScroll: 3,
+    	dots: false,
+  		speed: 300,
+  		infinite: true,
+    });
+});
