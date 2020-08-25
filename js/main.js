9c712a250a9ef34406e7dbebaf60d63950d2b2d2
freeua <freeua.artyom.martynov@gmail.com>
2020-08-25 18:12:41 +0300

    site state 25.08.2020

--- js/main.js
+++ js/main.js
@@ -5,5 +5,90 @@ $( document ).ready(function() {
     	dots: false,
   		speed: 300,
   		infinite: true,
+  		responsive: [
+    	 {
+	      breakpoint: 767,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+	      }
+	    ]
     });
+
+    $('.customers-selling-section__products-wrapper').slick({
+    	slidesToShow: 4,
+  		slidesToScroll: 4,
+    	dots: false,
+  		speed: 300,
+  		infinite: true,
+  		variableWidth:true,
+  		prevArrow: $('.prev-slide'),
+    	nextArrow: $('.next-slide'),
+    	responsive: [
+	    	{
+		      breakpoint: 812,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3
+		      }
+		    },
+		    {
+		      breakpoint: 767,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    }
+		  ]
+    });
+
+    $('.unified-manufacturing-section__products-wrapper').slick({
+    	slidesToShow: 3,
+  		slidesToScroll: 3,
+    	dots: false,
+  		speed: 300,
+  		infinite: true,
+  		variableWidth:true,
+  		prevArrow: $('.unified-prev-slide'),
+    	nextArrow: $('.unified-next-slide'),
+    	responsive: [
+	    	{
+		      breakpoint: 812,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    }
+		]
+    });
+
+
+    $('.testimonials-section__testimonials-slider').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.testimonials-section__logos'
+	});
+
+	$('.testimonials-section__logos').slick({
+	  slidesToShow: 7,
+	  slidesToScroll: 1,
+	  infinite: true,
+	  asNavFor: '.testimonials-section__testimonials-slider',
+	  dots: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [
+		{
+	      breakpoint: 812,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+	});
+    
 });
