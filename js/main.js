7e79a63bd9a8d8f552c7431db4785a6b7cd67a51
freeua <freeua.artyom.martynov@gmail.com>
2020-08-26 21:01:11 +0300

    site state 26.08.2020

--- js/main.js
+++ js/main.js
@@ -81,14 +81,67 @@ $( document ).ready(function() {
 	  centerMode: true,
 	  focusOnSelect: true,
 	  responsive: [
-		{
-	      breakpoint: 812,
-	      settings: {
-	        slidesToShow: 5,
-	        slidesToScroll: 1
-	      }
-	    }
-	  ]
+	    	{
+		      breakpoint: 812,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 767,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
 	});
-    
+    	
+    // slider
+	  $slick_slider = $('.products-section__columns');
+	  settings_slider = {
+	    dots: false,
+	    arrows: true,
+	    prevArrow: $('.products-section-prev-slide'),
+    	nextArrow: $('.products-section-next-slide'),
+	    slidesToShow: 2,
+  		slidesToScroll: 2,
+  		speed: 300,
+  		infinite: true,
+  		variableWidth:true,
+	    // more settings
+	  }
+	  slick_on_mobile( $slick_slider, settings_slider);
+
+	// slick on mobile
+	  function slick_on_mobile(slider, settings){
+	    $(window).on('load resize', function() {
+	      if ($(window).width() >= 767) {
+	        if (slider.hasClass('slick-initialized')) {
+	          slider.slick('unslick');
+	        }
+	        return
+	      }
+	      if (!slider.hasClass('slick-initialized')) {
+	        return slider.slick(settings);
+	      }
+	    });
+	  };
+
+
+	$(window).scroll(function() {
+	    if($(window).scrollTop() > 10 ){
+	       $('.header__mobile-menu-wrapper').addClass('header__mobile-menu-fixed');
+	    } else  {
+	      $('.header__mobile-menu-wrapper').removeClass('header__mobile-menu-fixed');
+	    }
+	});
+
+	$('.footer__menu-wrapper .footer__menu-title').click(function() {
+		$(this).parents('.footer__menu-wrapper').find('.footer__nav').slideToggle();
+	});
+
+
+
 });
