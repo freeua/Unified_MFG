1320951c68b0dc6326c7fe86d71f4ecc726c0109
freeua <freeua.artyom.martynov@gmail.com>
2020-09-10 15:24:11 +0300

    item page adaptivity

--- js/main.js
+++ js/main.js
@@ -130,16 +130,39 @@ $(document).ready(function () {
 		]
 	});
 
-	// Item page. First slider
+	// Item page. First slider. Desktop. Full picture slider
+	$('.item-slider-wrapper__desk .slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav',
+		infinite: false,
+	});
+
+	// Item page. First slider. Desktop. Thumbnail slider
+	$('.item-slider-wrapper__desk .slider-nav').slick({
+		appendArrows: ".item__thumbnail-arrows",
+		infinite: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		vertical: true,
+		asNavFor: '.slider-for',
+		dots: false,
+		focusOnSelect: true,
+		verticalSwiping: true,
+	});
+
+	// Item page. First slider. Mobile slider
 	$('.item-slider__mob').slick({
+		infinite: false,
+		appendArrows: ".item-slider__mob-arrows",
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		dots: false,
 		speed: 300,
 		infinite: true,
 		variableWidth: true,
-		prevArrow: $('.prev-slide'),
-		nextArrow: $('.next-slide'),
 		responsive: [
 			{
 				breakpoint: 767,
@@ -225,19 +248,16 @@ $(document).ready(function () {
 		$(this).parents('.footer__menu-wrapper').find('.footer__nav').slideToggle();
 	});
 
-
 	$(".menu-collapsed").click(function (e) {
 		$(this).toggleClass("menu-expanded");
 	});
 
 	$(".collection__category-item>a").click(function (e) {
 		e.preventDefault();
-		//$('.collection__subcategories-list').hide()
 		$(".collection__category-item").removeClass("collection__category-item-active");
 		$(this).parent().toggleClass("collection__category-item-active");
 		$(this).parents('.collection__category-item').find('.collection__subcategories-list').slideToggle();
 		$(this).parents('.collection__category-item').find('a').toggleClass("active");
-
 	});
 
 	$(".collection__subcategory-item>a").click(function (e) {
@@ -268,7 +288,6 @@ $(document).ready(function () {
 		$(this).addClass('item-info__color-item_active');
 	})
 
-
 	// change active class when user clicks on material(item page)
 	$('.item-info__material-item').click(function () {
 		$('.item-info__material-item').removeClass('item-info__material-item_active');
@@ -277,16 +296,9 @@ $(document).ready(function () {
 
 	// change active class when user clicks on anchor(item page)
 	$('.item-info__anchor').click(function () {
-		$('.item-info__anchor').removeClass('item-info__anchor_active');
-		$(this).addClass('item-info__anchor_active');
+		$('.item-info__anchor').parent().removeClass('item-info__anchor_active');
+		$(this).parent().addClass('item-info__anchor_active');
 	})
-
-
-
-
-	// $(document).on("click", ".custom-print__input-file", function () {
-	// 	$(this).parent().find('input[type="file"]').trigger("click");
-	// });
 
 	// custom "download file" input. show file name of code user selected file
 	$(document).on("change", ".custom-print__input-file", function () {
@@ -307,22 +319,38 @@ $(document).ready(function () {
 		}, 500);
 	});
 
+	// Cache selectors
+	var topMenu = $(".item-info__anchors"),
+		topMenuHeight = topMenu.outerHeight() + 15,
+		// All list items
+		menuItems = topMenu.find("a"),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function () {
+			var item = $($(this).attr("href"));
+			if (item.length) { return item; }
+		});
 
-	$('.item__thumbnail-slider').slick({
-		slidesToShow: 2,
-		vertical: true,
-		slidesToScroll: 1,
-		speed: 300,
-		dots: true,
-		draggable: true,
-		infinite: false,
-		prevArrow: $('.prev-slide'),
-		nextArrow: $('.next-slide'),
+	// Bind to scroll
+	$(window).scroll(function () {
+		// Get container scroll position
+		var fromTop = $(this).scrollTop() + topMenuHeight;
 
+		// Get id of current scroll item
+		var cur = scrollItems.map(function () {
+			if ($(this).offset().top < fromTop)
+				return this;
+		});
+		// Get the id of the current element
+		cur = cur[cur.length - 1];
+		var id = cur && cur.length ? cur[0].id : "";
+		// Set/remove active class
+		menuItems
+			.parent().removeClass("item-info__anchor_active")
+			.end().filter("[href='#" + id + "']").parent().addClass("item-info__anchor_active");
 	});
 
-	$('.item__thumbnail-slide').click(function () {
-		console.log($(this).attr('data-open'));
-
+	$("#load-colors__btn").change(function () {
+		$('.item-info__color-item:nth-child(n+9)').toggle();
 	})
-});
+
+})
