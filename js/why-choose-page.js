7d4e74003fdf73c802d1ac93923c531c7fcdde80
freeua <freeua.artyom.martynov@gmail.com>
2020-09-23 17:38:29 +0300

    why choose page desktop and mobile

--- js/why-choose-page.js
+++ js/why-choose-page.js
@@ -2,41 +2,53 @@ $(document).ready(function () {
 	$('.why-choose-second-section__blog-slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		arrows: true,
-		prevArrow: $('.why-choose-second-section-prev-slide'),
-		nextArrow: $('.why-choose-second-section-next-slide'),
 		dots: false,
 		speed: 300,
 		infinite: false,
+		prevArrow: $('.why-choose-second-section-prev-slide'),
+		nextArrow: $('.why-choose-second-section-next-slide'),
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					variableWidth: true,
+				}
+			}
+		]
 	});
 
 	$('.why-choose-fourth-section__news-slider').slick({
 		slidesToShow: 2,
 		slidesToScroll: 1,
-		arrows: true,
-		prevArrow: $('.why-choose-fourth-section-prev-slide'),
-		nextArrow: $('.why-choose-fourth-section-next-slide'),
 		dots: false,
 		speed: 300,
 		infinite: false,
 		variableWidth: true,
+		prevArrow: $('.why-choose-fourth-section-prev-slide'),
+		nextArrow: $('.why-choose-fourth-section-next-slide'),
 	});
 
 	$('.why-choose-fifth-section__slider').slick({
 		slidesToShow: 5,
 		slidesToScroll: 1,
-		arrows: true,
-		prevArrow: $('.why-choose-fifth-section-prev-slide'),
-		nextArrow: $('.why-choose-fifth-section-next-slide'),
 		dots: false,
 		speed: 300,
 		infinite: false,
 		variableWidth: true,
+		prevArrow: $('.why-choose-fifth-section-prev-slide'),
+		nextArrow: $('.why-choose-fifth-section-next-slide'),
 		responsive: [
 			{
 				breakpoint: 1100,
 				settings: {
 					slidesToShow: 4,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 2,
 					slidesToScroll: 1
 				}
 			}
@@ -46,13 +58,21 @@ $(document).ready(function () {
 	$('.why-choose-sixth-section__reviews_slider').slick({
 		slidesToShow: 2,
 		slidesToScroll: 2,
-		arrows: true,
-		prevArrow: $('.why-choose-sixth-section-prev-slide'),
-		nextArrow: $('.why-choose-sixth-section-next-slide'),
 		dots: false,
 		speed: 300,
 		infinite: false,
 		variableWidth: true,
+		prevArrow: $('.why-choose-sixth-section-prev-slide'),
+		nextArrow: $('.why-choose-sixth-section-next-slide'),
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 
 })
